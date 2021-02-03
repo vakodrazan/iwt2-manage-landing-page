ff0ab8503ddcdc64fcc0c00f1ab64375d94455eb
Noeline Marie Jeanne <noeline.mar@onja.org>
2021-02-03 07:28:07 +0300

    Avoid overflow x with the body and add href with the anchor

--- dist/iwt2-manage-landing-page.e31bb0bc.js
+++ dist/iwt2-manage-landing-page.e31bb0bc.js
@@ -168,7 +168,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56124" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53003" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
